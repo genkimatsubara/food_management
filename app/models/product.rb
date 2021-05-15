464427b377276c0fadca713855fd89a4310ab254
Genki Matsubara <genki8210@icloud.com>
2021-05-15 16:02:44 +0000

    productsコントローラ・モデル作成

---
+++ app/models/product.rb
@@ -0,0 +1,12 @@
+class Product < ApplicationRecord
+  belongs_to :user
+  
+  validates :name, presence: true
+  validates :deadline, presence: true
+  validate :date_before_start
+
+
+  def date_before_start
+    errors.add(:deadline, "は今日以降のものを選択してください") if deadline.nil? || deadline < Date.today
+  end
+end

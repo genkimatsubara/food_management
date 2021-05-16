3586cc143e01363b06e81e2c09f0365dffc37770
Genki Matsubara <genki8210@icloud.com>
2021-05-16 03:42:38 +0000

    フードモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_15_155332) do
+ActiveRecord::Schema.define(version: 2021_05_16_034010) do
+
+  create_table "foods", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "image", null: false
+    t.string "url", null: false
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name", null: false
